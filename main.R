c417f904306d1456018c9f2e442265529b8cb1db
anibalb05 <anibalbrenesj@gmail.com>
2019-10-14 10:09:51 -0600

    prueba update
    add setwd

--- main.R
+++ main.R
@@ -2,6 +2,7 @@
 library(foreign)
 library(tibble)
 library(tidyverse)
+#setwd("C:/Users/abrenes/Downloads")
 data<- read.spss("./data/data.sav", to.data.frame=TRUE, use.value.labels = TRUE)
 attr(data, "variable.labels")
 summary(data)
