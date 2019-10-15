bb6f6b2e869c6c9de0472b30100cdc953a12960f
Fabian <fabian.hernandez@hulihealth.com>
2019-10-15 09:13:41 -0600

    Adding some descriptive analysis

--- main.R
+++ main.R
@@ -2,12 +2,49 @@
 library(foreign)
 library(tibble)
 library(tidyverse)
+library(ggplot2)
 #setwd("C:/Users/abrenes/Downloads")
 data<- read.spss("./data/data.sav", to.data.frame=TRUE, use.value.labels = TRUE)
 attr(data, "variable.labels")
 summary(data)
 names(data)
+view(data)
 
 # Change to tible table
+ 
 data<-as_tibble(data)
-data %>% summary()
+
+##################################################################################
+################################Descriptivos######################################
+##################################################################################
+# Viviendas por provincia
+data %>%
+  group_by(A1) %>% 
+  tally() %>% 
+  print(n = Inf)
+
+# Viviendas por canton
+data %>%
+  group_by(A1, A3) %>% 
+  tally() %>% 
+  print(n = Inf)
+
+# Viviendas por distrito
+data %>%
+  group_by(A1, A2, A3) %>% 
+  tally() %>% 
+  print(n = Inf)
+
+
+# Viviendas por zona
+data %>%
+  count(A5) %>% 
+  mutate(per=n/nrow(data)) %>% 
+  ggplot(aes(x= A5)) + 
+  geom_bar(aes(y = per,fill=A5), stat = "identity") +
+  geom_text(aes(label = scales::percent(per), y= per ), stat= "identity", vjust = -.5) +
+  labs(y = "", fill="Área", x="") +
+  scale_y_continuous(labels = scales::percent)
+
+
+
