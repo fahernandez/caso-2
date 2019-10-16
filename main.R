612f331f448606ab738a0d2d0073859d1a2151b4
anibalb05 <anibalbrenesj@gmail.com>
2019-10-15 23:02:34 -0600

    Update main.R
    Un poco más de descriptivas

--- main.R
+++ main.R
@@ -3,7 +3,7 @@ library(foreign)
 library(tibble)
 library(tidyverse)
 library(ggplot2)
-#setwd("C:/Users/abrenes/Downloads")
+#setwd("C:/Users/Anibal/Google Drive/Casos/Caso 2")
 data<- read.spss("./data/data.sav", to.data.frame=TRUE, use.value.labels = TRUE)
 attr(data, "variable.labels")
 summary(data)
@@ -20,9 +20,13 @@ data<-as_tibble(data)
 # Viviendas por provincia
 data %>%
   group_by(A1) %>% 
-  tally() %>% 
-  print(n = Inf)
-
+  tally() %>%
+  #print(n = Inf) %>% 
+  ggplot(aes(x = reorder(A1,-n), y = n)) + geom_bar(stat = "identity") +
+    theme_bw() + labs(x = "Provincia", y = "Viviendas", 
+                        title = "Cantidad de viviendas por provincia")
+ggsave("./viv_por_prov.png", units="cm", height = 8, width = 15.5)
+dev.off()
 # Viviendas por canton
 data %>%
   group_by(A1, A3) %>% 
@@ -47,4 +51,16 @@ data %>%
   scale_y_continuous(labels = scales::percent)
 
 
-
+# ingreso medio por provincia
+data %>%
+    group_by(Ingreso) %>% 
+    tally() %>%
+    #print(n = Inf)
+    mutate(Ing2 = factor(c("<500mil", "501-750 mil", 
+                    "751-1 mill", ">1 mill", "NR"), levels = c("<500mil", "501-750 mil", 
+                                                               "751-1 mill", ">1 mill", "NR"))) %>% 
+    
+ggplot(aes(x = Ing2, y = n)) + geom_bar(stat = "identity") +
+    theme_bw() + labs(x = "Nivel de ingreso", y = "Viviendas", 
+                      title = "Cantidad de viviendas por categoría de ingreso")
+ggsave("./ing_por_viv.png", units = "cm", height = 8, width = 15.5)
